dd33f8fa5963cd9343ab2f59e90b17a63edab1f1
phantrong001 <phantrong001@gmail.com>
2022-11-01 22:21:43 +0700

    fix-deploy-heroku

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
+import 'styles/index.scss';
 import 'helper/i18n';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from 'reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,7 +13,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+reportWebVitals();
